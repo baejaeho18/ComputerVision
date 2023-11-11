9dcf65bb196abb581e8d00249400b11bbe38c96f
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-11-11 17:13:15 +0900

    Update project2.cpp

--- assignments/project2.cpp
+++ assignments/project2.cpp
@@ -1,11 +1,14 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 using namespace cv;
 
-void lookROI(Mat frame, Rect left, Rect center, Rect right);
-void drawHoughLine(Mat frame, vector<Vec2f> line);
+void lookROI(Mat frame, Rect left, Rect center, Rect right, Rect top);
+void drawLeftHoughLine(Mat frame, vector<Vec2f> lines, Rect roi);
+void drawCenterHoughLine(Mat frame, vector<Vec2f> lines, Rect roi);
+void drawRightHoughLine(Mat frame, vector<Vec2f> lines, Rect roi);
 
 void printLaneDepartureAlarm(Mat frame);
 void printMovingCarAlarm(Mat frame);
@@ -25,17 +28,23 @@ int main()
 	}
 	delay = 1000 / cap.get(CAP_PROP_FPS);
 	// Lane
-	Rect left(150, 200, 150, 250);
-	Rect center(300, 200, 150, 250);
-	Rect right(450, 200, 150, 250);
+	Rect left(150, 280, 150, 200);
+	Rect center(250, 250, 200, 200);
+	Rect right(400, 280, 150, 200);
 	Mat left_roi, center_roi, right_roi;
 	Mat left_canny, center_canny, right_canny;
 	Mat frame_copy;
 	vector<Vec2f> left_line;
 	vector<Vec2f> center_line;
 	vector<Vec2f> right_line;
-	// Moving
+	vector<int> frameBuffer(21, 0);
+	int onesCount;
 
+	// Moving
+	Rect top(0, 0, 720, 200);
+	Rect front_car(230, 250, 200, 200);
+	Mat background, car_roi;
+	Ptr<BackgroundSubtractor> pMOG2 = createBackgroundSubtractorMOG2();
 
 	while (1)
 	{
@@ -49,6 +58,7 @@ int main()
 		frame_copy = frame.clone();
 
 		// Lane Departure
+		onesCount = 0;
 		left_roi = frame_copy(left);
 		right_roi = frame_copy(right);
 		cvtColor(left_roi, left_roi, CV_BGR2GRAY);
@@ -57,8 +67,8 @@ int main()
 		blur(right_roi, right_canny, Size(5, 5));
 		Canny(left_canny, left_canny, 10, 70, 3);
 		Canny(right_canny, right_canny, 10, 70, 3);
-		HoughLines(left_canny, left_line, 1, (CV_PI / 180), 60, 0, 0, 30 * (CV_PI / 180), 60 * (CV_PI / 180));
-		HoughLines(right_canny, right_line, 1, (CV_PI / 180), 60, 0, 0, 30 * (CV_PI / 180), 60 * (CV_PI / 180));
+		HoughLines(left_canny, left_line, 1, (CV_PI / 180), 60, 0, 0, 20 * (CV_PI / 180), 60 * (CV_PI / 180));
+		HoughLines(right_canny, right_line, 1, (CV_PI / 180), 60, 0, 0, 120 * (CV_PI / 180), 150 * (CV_PI / 180));
 
 		if (left_line.empty() && right_line.empty())
 		{
@@ -66,15 +76,39 @@ int main()
 			cvtColor(center_roi, center_roi, CV_BGR2GRAY);
 			blur(center_roi, center_canny, Size(5, 5));
 			Canny(center_canny, center_canny, 10, 70, 3);
-			HoughLines(center_canny, center_line, 1, (CV_PI / 180), 65, 0, 0, 150 * (CV_PI / 180), 177 * (CV_PI / 180));
+			HoughLines(center_canny, center_line, 1, (CV_PI / 180), 60, 0, 0, 150 * (CV_PI / 180), 220 * (CV_PI / 180));
 			if (!center_line.empty())
-				printLaneDepartureAlarm(frame);
+			{
+				//printLaneDepartureAlarm(frame);
+				cout << 1 << endl;
+				frameBuffer.push_back(1);
+			}
+			else
+			{
+				cout << 0 << endl;
+				frameBuffer.push_back(0);
+			}
 		}
+		else {
+			frameBuffer.push_back(0);
+		}
+		frameBuffer.erase(frameBuffer.begin());
+		onesCount = count(frameBuffer.begin(), frameBuffer.end(), 1);
+		if (onesCount >= 5)
+		{
+			printLaneDepartureAlarm(frame);
+			cout << "Lane Departure" << endl;
+		}
+			
+		lookROI(frame, left, center, right, top);
+		drawLeftHoughLine(frame, left_line, left);
+		drawCenterHoughLine(frame, center_line, center);
+		drawRightHoughLine(frame, right_line, right);
 
-		lookROI(frame, left, center, right);
-		drawHoughLine(frame, left_line);
-		drawHoughLine(frame, right_line);
 		// Car Moving
+		background = frame_copy(top);
+		car_roi = frame_copy(front_car);
+		cvtColor(background, background, CV_BGR2GRAY);
 
 
 
@@ -86,20 +120,21 @@ int main()
 
 void printLaneDepartureAlarm(Mat frame)
 {
-	putText(frame, format("Lane Departure!"), Point(frame.cols / 2, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
+	putText(frame, format("Lane Departure!"), Point(frame.cols / 2 - 240, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
 }
 void printMovingCarAlarm(Mat frame)
 {
 	putText(frame, format("Car is Moving!"), Point(frame.cols / 2, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
 }
 
-void lookROI(Mat frame, Rect left, Rect center, Rect right)
+void lookROI(Mat frame, Rect left, Rect center, Rect right, Rect top)
 {
 	rectangle(frame, left, Scalar(0, 0, 122), 2);
 	rectangle(frame, center, Scalar(0, 0, 122), 2);
 	rectangle(frame, right, Scalar(0, 0, 122), 2);
+	rectangle(frame, top, Scalar(0, 0, 122), 2);
 }
-void drawHoughLine(Mat frame, vector<Vec2f> lines)
+void drawLeftHoughLine(Mat frame, vector<Vec2f> lines, Rect roi)
 {
 	for (size_t i = 0; i < lines.size(); i++)
 	{
@@ -107,10 +142,40 @@ void drawHoughLine(Mat frame, vector<Vec2f> lines)
 		float theta = lines[i][1];
 		double a = cos(theta);
 		double b = sin(theta);
-		double x0 = a * rho;
-		double y0 = b * rho;
+		double x0 = a * rho + roi.x;
+		double y0 = b * rho + roi.y;
 		Point pt1(cvRound(x0 + 1000 * (-b)), cvRound(y0 + 1000 * (a)));
 		Point pt2(cvRound(x0 - 1000 * (-b)), cvRound(y0 - 1000 * (a)));
-		line(frame, pt1, pt2, Scalar(0, 255, 0), 3, 8, 0);
+		line(frame, pt1, pt2, Scalar(0, 145, 0), 1, 8, 0);
+	}
+}
+void drawCenterHoughLine(Mat frame, vector<Vec2f> lines, Rect roi)
+{
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		float rho = lines[i][0];
+		float theta = lines[i][1];
+		double a = cos(theta);
+		double b = sin(theta);
+		double x0 = a * rho + roi.x;
+		double y0 = b * rho + roi.y;
+		Point pt1(cvRound(x0 + 1000 * (-b)), cvRound(y0 + 1000 * (a)));
+		Point pt2(cvRound(x0 - 1000 * (-b)), cvRound(y0 - 1000 * (a)));
+		line(frame, pt1, pt2, Scalar(145, 0, 0), 1, 8, 0);
+	}
+}
+void drawRightHoughLine(Mat frame, vector<Vec2f> lines, Rect roi)
+{
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		float rho = lines[i][0];
+		float theta = lines[i][1];
+		double a = cos(theta);
+		double b = sin(theta);
+		double x0 = a * rho + roi.x;
+		double y0 = b * rho + roi.y;
+		Point pt1(cvRound(x0 + 1000 * (-b)), cvRound(y0 + 1000 * (a)));
+		Point pt2(cvRound(x0 - 1000 * (-b)), cvRound(y0 - 1000 * (a)));
+		line(frame, pt1, pt2, Scalar(145, 145, 0), 1, 8, 0);
 	}
 }
