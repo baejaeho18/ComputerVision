e289b0db41c2e370c4213b3345d7bd760d3d3770
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-12-18 23:36:40 +0900

    refactoring

--- assignments/project4.cpp
+++ assignments/project4.cpp
@@ -11,61 +11,118 @@ using namespace std;
 using namespace cv;
 
 // ORB 기반의 특징점 검출 및 서술자 추출
-void detectAndDescribe(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors) {
+void detectAndDescribe(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors);
+// 특징점 매칭
+void matchFeatures(const Mat& desc1, const Mat& desc2, vector<DMatch>& matches);
+// 호모그래피 행렬 계산
+Mat computeHomography(const vector<KeyPoint>& keypoints1, const vector<KeyPoint>& keypoints2, const vector<DMatch>& matches);
+// 이미지 변환 및 스티칭
+Mat warpAndStitch(const Mat& image1, const Mat& image2, const Mat& H);
+Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H);
+
+int main()
+{
+    vector<Mat> images;
+    vector<Mat> descriptors_list;
+    vector<vector<KeyPoint>> keypoints_list;
+    vector<string> filenames = { "pano1.jpg", "pano2.jpg", "pano3.jpg", "pano4.jpg" };
+
+    // 이미지 로딩 및 특징점 검출과 서술자 추출
+    for (const string& filename : filenames) 
+    {
+        Mat image = imread(filename, IMREAD_COLOR);
+        if (image.empty()) 
+        {
+            cout << "Image read Error: " << filename << endl;
+            return -1;
+        }
+        images.push_back(image);
+
+        vector<KeyPoint> keypoints;
+        Mat descriptors;
+        detectAndDescribe(image, keypoints, descriptors);
+        keypoints_list.push_back(keypoints);
+        descriptors_list.push_back(descriptors);
+    }
+
+    // 첫 번째 이미지를 기준으로 설정
+    Mat panorama = images[0];
+    Mat accumulatedHomography = Mat::eye(3, 3, CV_64F);
+
+    // 나머지 이미지들과 스티칭
+    for (size_t i = 1; i < images.size(); i++) 
+    {
+        vector<DMatch> matches;
+        matchFeatures(descriptors_list[i - 1], descriptors_list[i], matches);
+        Mat H = computeHomography(keypoints_list[i - 1], keypoints_list[i], matches);
+        accumulatedHomography = H * accumulatedHomography;
+        panorama = stitchTwoImages(panorama, images[i], accumulatedHomography);
+    }
+
+    // 결과 출력
+    imshow("Panorama", panorama);
+    imwrite("Panorama.jpg", panorama);
+    waitKey(0);
+
+    return 0;
+}
+
+// ORB 기반의 특징점 검출 및 서술자 추출
+void detectAndDescribe(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors)
+{
     Ptr<ORB> orb = ORB::create(1000);
     orb->detectAndCompute(image, Mat(), keypoints, descriptors);
 }
 
-// 특징점 매칭 (Brute-Force Matcher)
-void matchFeatures(const Mat& desc1, const Mat& desc2, vector<DMatch>& matches) {
+// 특징점 매칭
+void matchFeatures(const Mat& desc1, const Mat& desc2, vector<DMatch>& matches)
+{
     Ptr<BFMatcher> matcher = BFMatcher::create(NORM_HAMMING);
     vector<vector<DMatch>> knn_matches;
-    matcher->knnMatch(desc1, desc2, knn_matches, 2); // 2 for knn
-    // 필터링: Lowe's ratio test
+    matcher->knnMatch(desc1, desc2, knn_matches, 2);
+    // 필터링
     const float ratio_thresh = 0.7f;
-    for (size_t i = 0; i < knn_matches.size(); i++) {
-        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance) {
+    for (size_t i = 0; i < knn_matches.size(); i++)
+        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
             matches.push_back(knn_matches[i][0]);
-        }
-    }
 }
 
 // 호모그래피 행렬 계산
-Mat computeHomography(const vector<KeyPoint>& keypoints1, const vector<KeyPoint>& keypoints2, const vector<DMatch>& matches) {
+Mat computeHomography(const vector<KeyPoint>& keypoints1, const vector<KeyPoint>& keypoints2, const vector<DMatch>& matches)
+{
     vector<Point2f> points1, points2;
     for (const auto& match : matches) {
         points1.push_back(keypoints1[match.queryIdx].pt);
         points2.push_back(keypoints2[match.trainIdx].pt);
     }
-    return findHomography(points2, points1, RANSAC); // 주의: 순서 변경
+    return findHomography(points2, points1, RANSAC);
 }
 
 // 이미지 변환 및 스티칭
-Mat warpAndStitch(const Mat& image1, const Mat& image2, const Mat& H) {
-    // Create a black image that will fit both the images
+Mat warpAndStitch(const Mat& image1, const Mat& image2, const Mat& H)
+{
     Mat result;
     warpPerspective(image2, result, H, Size(image1.cols + image2.cols, image1.rows));
-    // Copy image1 on the first half of full image
     Mat half(result, Rect(0, 0, image1.cols, image1.rows));
     image1.copyTo(half);
     return result;
 }
 
-Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H) {
-    // 이미지 변환 시 결과 이미지의 크기를 계산
+Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H)
+{
+    // 결과 이미지의 크기
     vector<Point2f> corners(4);
     corners[0] = Point2f(0, 0);
     corners[1] = Point2f((float)image.cols, 0);
     corners[2] = Point2f((float)image.cols, (float)image.rows);
     corners[3] = Point2f(0, (float)image.rows);
-
-    // 변환된 코너 포인트 계산
     vector<Point2f> transformed_corners(4);
     perspectiveTransform(corners, transformed_corners, H);
 
-    // 변환된 이미지가 들어갈 수 있는 최소 사이즈를 계산
+    // 변환된 이미지의 최소 사이즈를 계산
     double max_x = 0, max_y = 0;
-    for (const auto& pt : transformed_corners) {
+    for (const auto& pt : transformed_corners)
+    {
         max_x = max(max_x, (double)pt.x);
         max_y = max(max_y, (double)pt.y);
     }
@@ -95,9 +152,12 @@ Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H) {
     bitwise_and(warpedMaskNew, warpedMaskPanorama, overlapMask);
 
     // Perform blending
-    for (int y = 0; y < result.rows; ++y) {
-        for (int x = 0; x < result.cols; ++x) {
-            if (overlapMask.at<uchar>(y, x)) {
+    for (int y = 0; y < result.rows; ++y)
+    {
+        for (int x = 0; x < result.cols; ++x)
+        {
+            if (overlapMask.at<uchar>(y, x))
+            {
                 float alpha = 0.5; // Simple average for blending
                 Vec3b colorNew = warpedImage.at<Vec3b>(y, x);
                 Vec3b colorPanorama = result.at<Vec3b>(y, x);
@@ -105,53 +165,8 @@ Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H) {
             }
         }
     }
-
     // Copy non-overlapped regions from the new image to the result
     warpedImage.copyTo(result, warpedMaskNew);
 
     return result;
-}
-
-int main() 
-{
-    vector<string> filenames = { "pano1.jpg", "pano2.jpg", "pano3.jpg", "pano4.jpg" };
-    vector<Mat> images;
-    vector<vector<KeyPoint>> keypoints_list;
-    vector<Mat> descriptors_list;
-
-    // 이미지 로딩 및 특징점 검출과 서술자 추출
-    for (const auto& filename : filenames) {
-        Mat image = imread(filename, IMREAD_COLOR);
-        if (image.empty()) {
-            cout << "Could not read the image: " << filename << endl;
-            return -1;
-        }
-        images.push_back(image);
-
-        vector<KeyPoint> keypoints;
-        Mat descriptors;
-        detectAndDescribe(image, keypoints, descriptors);
-        keypoints_list.push_back(keypoints);
-        descriptors_list.push_back(descriptors);
-    }
-
-    // 첫 번째 이미지를 기준으로 설정
-    Mat panorama = images[0];
-    Mat accumulatedHomography = Mat::eye(3, 3, CV_64F);
-
-    // 나머지 이미지들과 스티칭
-    for (size_t i = 1; i < images.size(); ++i) {
-        vector<DMatch> matches;
-        matchFeatures(descriptors_list[i - 1], descriptors_list[i], matches);
-        Mat H = computeHomography(keypoints_list[i - 1], keypoints_list[i], matches);
-        accumulatedHomography = H * accumulatedHomography;
-        panorama = stitchTwoImages(panorama, images[i], accumulatedHomography);
-    }
-
-    // 결과 출력
-    imshow("Panorama", panorama);
-    imwrite("Panorama.jpg", panorama);
-    waitKey(0);
-
-    return 0;
 }
