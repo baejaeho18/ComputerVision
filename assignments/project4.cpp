f1fadee98e5c08ca9721f88ff012c68dc0ce60cf
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-12-19 23:54:16 +0900

    refactoring

--- assignments/project4.cpp
+++ assignments/project4.cpp
@@ -10,76 +10,73 @@
 using namespace std;
 using namespace cv;
 
-// ORB 기반의 특징점 검출 및 서술자 추출
-void detectAndDescribe(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors);
-// 특징점 매칭
-void matchFeatures(const Mat& desc1, const Mat& desc2, vector<DMatch>& matches);
-// 호모그래피 행렬 계산
+// ORB 기반 특징점 검출 및 descriptors 계산
+void getDescriptors(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors);
+// 두 이미지의 descriptor간의 매칭
+void matchDescriptors(const Mat& descriptors1, const Mat& descriptors2, vector<DMatch>& matches);
+// RANSAC 기반 두 이미지 간의 homographyomography 행렬 계산
 Mat computeHomography(const vector<KeyPoint>& keypoints1, const vector<KeyPoint>& keypoints2, const vector<DMatch>& matches);
-// 이미지 변환 및 스티칭
-Mat warpAndStitch(const Mat& image1, const Mat& image2, const Mat& H);
-Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H);
+// 두 이미지를 병합
+Mat merge(const Mat& image1, const Mat& image2, const Mat& homography);
+// 파노라마에 연결
+Mat mergePanorama(const Mat& panorama, const Mat& image, const Mat& homography);
 
 int main()
 {
-    vector<Mat> images;
-    vector<Mat> descriptors_list;
-    vector<vector<KeyPoint>> keypoints_list;
-    vector<string> filenames = { "pano1.jpg", "pano2.jpg", "pano3.jpg", "pano4.jpg" };
+    Mat pano;
+    Mat homography = Mat::eye(3, 3, CV_64F);
+    vector<DMatch> matches;
 
-    // 이미지 로딩 및 특징점 검출과 서술자 추출
-    for (const string& filename : filenames) 
-    {
-        Mat image = imread(filename, IMREAD_COLOR);
-        if (image.empty()) 
-        {
-            cout << "Image read Error: " << filename << endl;
-            return -1;
-        }
-        images.push_back(image);
+    Mat pano1= imread("pano1.jpg");
+    vector<KeyPoint> keypoints1;
+    Mat descriptors1;
+    getDescriptors(pano1, keypoints1, descriptors1);
+    pano = pano1;
 
-        vector<KeyPoint> keypoints;
-        Mat descriptors;
-        detectAndDescribe(image, keypoints, descriptors);
-        keypoints_list.push_back(keypoints);
-        descriptors_list.push_back(descriptors);
-    }
+    Mat pano2 = imread("pano2.jpg");
+    vector<KeyPoint> keypoints2;
+    Mat descriptors2;
+    getDescriptors(pano2, keypoints2, descriptors2);
+    matchDescriptors(descriptors1, descriptors2, matches);
+    homography *= computeHomography(keypoints1, keypoints2, matches);
+    pano = mergePanorama(pano, pano2, homography);
 
-    // 첫 번째 이미지를 기준으로 설정
-    Mat panorama = images[0];
-    Mat accumulatedHomography = Mat::eye(3, 3, CV_64F);
+    Mat pano3 = imread("pano3.jpg");
+    vector<KeyPoint> keypoints3;
+    Mat descriptors3;
+    getDescriptors(pano3, keypoints3, descriptors3);
+    matchDescriptors(descriptors2, descriptors3, matches);
+    homography *= computeHomography(keypoints2, keypoints3, matches);
+    pano = mergePanorama(pano, pano3, homography);
 
-    // 나머지 이미지들과 스티칭
-    for (size_t i = 1; i < images.size(); i++) 
-    {
-        vector<DMatch> matches;
-        matchFeatures(descriptors_list[i - 1], descriptors_list[i], matches);
-        Mat H = computeHomography(keypoints_list[i - 1], keypoints_list[i], matches);
-        accumulatedHomography = H * accumulatedHomography;
-        panorama = stitchTwoImages(panorama, images[i], accumulatedHomography);
-    }
+    Mat pano4 = imread("pano4.jpg");
+    vector<KeyPoint> keypoints4;
+    Mat descriptors4;
+    getDescriptors(pano4, keypoints4, descriptors4);
+    matchDescriptors(descriptors3, descriptors4, matches);
+    homography *= computeHomography(keypoints3, keypoints4, matches);
+    pano = mergePanorama(pano, pano4, homography);
 
-    // 결과 출력
-    imshow("Panorama", panorama);
-    imwrite("Panorama.jpg", panorama);
+    imshow("Panorama", pano);
+    imwrite("Panorama.jpg", pano);
     waitKey(0);
 
     return 0;
 }
 
 // ORB 기반의 특징점 검출 및 서술자 추출
-void detectAndDescribe(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors)
+void getDescriptors(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors)
 {
     Ptr<ORB> orb = ORB::create(1000);
     orb->detectAndCompute(image, Mat(), keypoints, descriptors);
 }
 
 // 특징점 매칭
-void matchFeatures(const Mat& desc1, const Mat& desc2, vector<DMatch>& matches)
+void matchDescriptors(const Mat& descriptors1, const Mat& descriptors2, vector<DMatch>& matches)
 {
     Ptr<BFMatcher> matcher = BFMatcher::create(NORM_HAMMING);
     vector<vector<DMatch>> knn_matches;
-    matcher->knnMatch(desc1, desc2, knn_matches, 2);
+    matcher->knnMatch(descriptors1, descriptors2, knn_matches, 2);
     // 필터링
     const float ratio_thresh = 0.7f;
     for (size_t i = 0; i < knn_matches.size(); i++)
@@ -99,16 +96,16 @@ Mat computeHomography(const vector<KeyPoint>& keypoints1, const vector<KeyPoint>
 }
 
 // 이미지 변환 및 스티칭
-Mat warpAndStitch(const Mat& image1, const Mat& image2, const Mat& H)
+Mat merge(const Mat& image1, const Mat& image2, const Mat& homography)
 {
     Mat result;
-    warpPerspective(image2, result, H, Size(image1.cols + image2.cols, image1.rows));
+    warpPerspective(image2, result, homography, Size(image1.cols + image2.cols, image1.rows));
     Mat half(result, Rect(0, 0, image1.cols, image1.rows));
     image1.copyTo(half);
     return result;
 }
 
-Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H)
+Mat mergePanorama(const Mat& panorama, const Mat& image, const Mat& homography)
 {
     // 결과 이미지의 크기
     vector<Point2f> corners(4);
@@ -117,7 +114,7 @@ Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H)
     corners[2] = Point2f((float)image.cols, (float)image.rows);
     corners[3] = Point2f(0, (float)image.rows);
     vector<Point2f> transformed_corners(4);
-    perspectiveTransform(corners, transformed_corners, H);
+    perspectiveTransform(corners, transformed_corners, homography);
 
     // 변환된 이미지의 최소 사이즈를 계산
     double max_x = 0, max_y = 0;
@@ -128,7 +125,7 @@ Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H)
     }
     Size result_size = Size(max((int)max_x, panorama.cols), max((int)max_y, panorama.rows));
     Mat warpedImage;
-    warpPerspective(image, warpedImage, H, result_size);
+    warpPerspective(image, warpedImage, homography, result_size);
 
     // Create an empty panorama image
     Mat result = Mat::zeros(result_size, panorama.type());
@@ -144,8 +141,8 @@ Mat stitchTwoImages(const Mat& panorama, const Mat& image, const Mat& H)
 
     // Warp the masks
     Mat warpedMaskNew, warpedMaskPanorama;
-    warpPerspective(maskNew, warpedMaskNew, H, result_size);
-    warpPerspective(maskPanorama, warpedMaskPanorama, H, result_size);
+    warpPerspective(maskNew, warpedMaskNew, homography, result_size);
+    warpPerspective(maskPanorama, warpedMaskPanorama, homography, result_size);
 
     // Calculate the overlap region mask
     Mat overlapMask;
